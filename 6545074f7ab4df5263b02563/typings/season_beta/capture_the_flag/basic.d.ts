ee115589259afb32ef3db659a72e6bffac9f8fd8
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2024-03-28 21:10:52 +0000

    ✨ season beta capture the flag basic

---
+++ 6545074f7ab4df5263b02563/typings/season_beta/capture_the_flag/basic.d.ts
@@ -0,0 +1,4 @@
+declare module "arena/season_beta/capture_the_flag/basic" {
+    export * from "arena/season_beta/capture_the_flag/basic/prototypes";
+    export * from "arena/season_beta/capture_the_flag/basic/constants";
+}
