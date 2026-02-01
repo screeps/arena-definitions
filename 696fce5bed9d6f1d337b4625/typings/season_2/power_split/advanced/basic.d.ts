3e787d417e20753428822880dc2f0541362cf73b
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2026-02-01 13:01:00 +0000

    ✨ season 2: power split

---
+++ 696fce5bed9d6f1d337b4625/typings/season_2/power_split/advanced/basic.d.ts
@@ -0,0 +1,4 @@
+declare module "arena/season_2/power_split/advanced" {
+    export * from "arena/season_2/power_split/advanced/prototypes";
+    export * from "arena/season_2/power_split/advanced/constants";
+}
