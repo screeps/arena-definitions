4e83cb2f114d60368e416d7056e529c5ee2509a3
Nikolay Kharitonov <o4kapuk@o4kapuk.ru>
2026-02-01 12:00:44 +0000

    ✨ season 2: capture the flag

---
+++ 696fce5bed9d6f1d337b4622/main.mjs
@@ -0,0 +1,8 @@
+import { } from 'game/utils';
+import { } from 'game/prototypes';
+import { } from 'game/constants';
+import { } from 'arena/season_beta/capture_the_flag/basic';
+
+export function loop() {
+    // Your code goes here
+}
---
+++ 696fce5bed9d6f1d337b4623/typings/season_2/capture_the_flag/advanced.d.ts
@@ -0,0 +1,4 @@
+declare module "arena/season_2/capture_the_flag/advanced" {
+    export * from "arena/season_2/capture_the_flag/advanced/prototypes";
+    export * from "arena/season_2/capture_the_flag/advanced/constants";
+}
